aca1e5941f7fe46cca7101ebecdc2e77e0be4025
ajay-grappus <ajay@grappus.com>
2016-12-19 02:23:42 +0530

    To Do : API Client networking in a single file keyboard show move screen up

---
+++ On The Map/Loader.swift
@@ -0,0 +1,29 @@
+////
+////  Loader.swift
+////  On The Map
+////
+////  Created by Ajay Mann on 19/12/16.
+////  Copyright © 2016 Ajay Mann. All rights reserved.
+////
+//
+//import Foundation
+//import UIKit
+//
+//let indicator: UIActivityIndicatorView = UIActivityIndicatorView  (activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
+//
+//func showActivityIndicator() {
+//    indicator.color = UIColor.white
+//    indicator.backgroundColor = UIColor.darkGray
+//    indicator.alpha = 0.5
+//    indicator.frame = CGRect.init(x: 0.0, y: 0.0, width: 40.0, height: 40.0)
+//    indicator.layer.cornerRadius = 5
+//    indicator.center = view.center
+//    view.addSubview(indicator)
+//    indicator.bringSubview(toFront: view)
+//    indicator.startAnimating()
+//}
+//
+//func hideActivityIndicator(){
+//    indicator.stopAnimating()
+//    self.indicator.hidesWhenStopped = true
+//}
--- On The Map/SubmitLocationViewController.swift
+++ On The Map/SubmitLocationViewController.swift
@@ -16,16 +16,19 @@ class SubmitLocationViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    let indicator:UIActivityIndicatorView = UIActivityIndicatorView  (activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
     
     var locationText: String? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
         if let locationText = locationText {
+            self.showActivityIndicator()
             showLocationOnMap(locationText: locationText)
         }
     }
@@ -35,50 +38,45 @@ class SubmitLocationViewController: UIViewController {
     }
     
     @IBAction func submitPressed(_ sender: Any) {
-        appDelegate.mediaURL = mediaURLLink.text
-        let request = NSMutableURLRequest(url: NSURL(string: "https://parse.udacity.com/parse/classes/StudentLocation")! as URL)
-        request.httpMethod = "POST"
-        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
-        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = "{\"uniqueKey\": \"\(appDelegate.uniqueKey!)\", \"firstName\": \"\(appDelegate.firstName!)\", \"lastName\": \"\(appDelegate.lastName!)\",\"mapString\": \"\(locationText!)\", \"mediaURL\": \"\(appDelegate.mediaURL!)\",\"latitude\": \(appDelegate.latitude!), \"longitude\": \(appDelegate.longitude!)}".data(using: String.Encoding.utf8)
-
-        let session = URLSession.shared
-        let task = session.dataTask(with: request as URLRequest) { data, response, error in
-            if error != nil { // Handle error…
-                return
-            }
+        
+        if Reachability.isConnectedToNetwork() {
             
-            if let httpResponse = response as? HTTPURLResponse {
-                if httpResponse.statusCode > 199 && httpResponse.statusCode < 300 {
-                    performUIUpdatesOnMain {
-                        let alert = UIAlertController(title: "Post Successful", message: "Pin has been posted", preferredStyle: UIAlertControllerStyle.alert)
-                        alert.addAction(UIAlertAction(title: "Done", style: UIAlertActionStyle.default, handler: { action in
-                            self.performSegue(withIdentifier: "unwind", sender: nil)
-
-                        }))
-                        self.present(alert, animated: true, completion: nil)
-                        
-                    }
-                    
-                    
-                }
-            }
+            appDelegate.mediaURL = mediaURLLink.text
+            let request = NSMutableURLRequest(url: NSURL(string: "https://parse.udacity.com/parse/classes/StudentLocation")! as URL)
+            request.httpMethod = "POST"
+            request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+            request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpBody = "{\"uniqueKey\": \"\(appDelegate.uniqueKey!)\", \"firstName\": \"\(appDelegate.firstName!)\", \"lastName\": \"\(appDelegate.lastName!)\",\"mapString\": \"\(locationText!)\", \"mediaURL\": \"\(appDelegate.mediaURL!)\",\"latitude\": \(appDelegate.latitude!), \"longitude\": \(appDelegate.longitude!)}".data(using: String.Encoding.utf8)
             
-            var parsedResults: [String: AnyObject]
-
-            do {
-                parsedResults = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [String: AnyObject]
-                if let objectId = parsedResults["objectId"] as? String {
-                    
+            let session = URLSession.shared
+            let task = session.dataTask(with: request as URLRequest) { data, response, error in
+                if error != nil { // Handle error…
+                    return
                 }
                 
-            } catch {
-                let userInfo = [NSLocalizedDescriptionKey : "Could not parse the data as JSON: '\(data)'"]
+                if let httpResponse = response as? HTTPURLResponse {
+                    if httpResponse.statusCode > 199 && httpResponse.statusCode < 300 {
+                        performUIUpdatesOnMain {
+                            let alert = UIAlertController(title: "Post Successful", message: "Pin has been posted", preferredStyle: UIAlertControllerStyle.alert)
+                            alert.addAction(UIAlertAction(title: "Done", style: UIAlertActionStyle.default, handler: { action in
+                                self.performSegue(withIdentifier: "unwind", sender: nil)
+                                
+                            }))
+                            self.present(alert, animated: true, completion: nil)
+                        }
+                    }
+                }
             }
+            task.resume()
         }
-        task.resume()
+        else {
+                let alert = UIAlertController(title: "No Internet", message: "No Internet Connection", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Done", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+        }
     }
+    
 }
 
 extension SubmitLocationViewController: MKMapViewDelegate {
@@ -108,9 +106,28 @@ extension SubmitLocationViewController: MKMapViewDelegate {
             let pinAnnotationView = MKPinAnnotationView(annotation: pointAnnotation, reuseIdentifier: nil)
             self.mapView.centerCoordinate = pointAnnotation.coordinate
             performUIUpdatesOnMain {
+                self.hideActivityIndicator()
                 self.mapView.addAnnotation(pinAnnotationView.annotation!)
+                
             }
             
         }
+    }
+    
+    func showActivityIndicator() {
+        indicator.color = UIColor.white
+        indicator.backgroundColor = UIColor.darkGray
+        indicator.alpha = 0.5
+        indicator.frame = CGRect.init(x: 0.0, y: 0.0, width: 40.0, height: 40.0)
+        indicator.layer.cornerRadius = 5
+        indicator.center = self.view.center
+        self.view.addSubview(indicator)
+        indicator.bringSubview(toFront: self.view)
+        indicator.startAnimating()
+    }
+    
+    func hideActivityIndicator(){
+        indicator.stopAnimating()
+        self.indicator.hidesWhenStopped = true
     }
 }
